63a22582691729e28efe079616525131d8870d2b
Viktor M <mikhalcheskiu@gmail.com>
2023-12-26 20:28:37 +0200

    skills add images and make test form sending

--- contactMeContent.js
+++ contactMeContent.js
@@ -1,6 +1,6 @@
 export let  contactMeContent =`
 <section class="contact-me-tab _box">
-                <form action="" class="contact-me">
+                <form action="" class="contact-me  ">
                     <div class="contact-me__inputs-box">
                     <div class="contact-me__info">
                         <p class="contact-me__input-title">Name</p>
@@ -20,7 +20,7 @@ export let  contactMeContent =`
                     </div>
                     <div class="contact-me__info _request-topic">
                         <label for="request-topic" class="contact-me__input-title">Request Topic</label>
-                        <select id="request-topic" name="request-topic">
+                        <select id="request-topic" class="request-topic__select">
                             <option value="Work Proposal">Work Proposal</option>
                             <option value="Problem">Problem</option>
                             <option value="Money back">Money back</option>

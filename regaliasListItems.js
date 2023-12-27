280641b6581cd2b4b9f10a3f18b07ffb4986353a
Viktor M <mikhalcheskiu@gmail.com>
2023-12-27 07:25:42 +0200

    add more certeficates

--- regaliasListItems.js
+++ regaliasListItems.js
@@ -1,6 +1,22 @@
 export let regaliasListItems = [
     {
         "id":1,
+        "title":"Udemy Certificate",
+        "caption":"Udemy Cer",
+        "icon-src":"./images/inventory/Scroll.png",
+        "full-img-src":"./images/inventory/UdemySerteficate.jpg",
+
+    },
+    {
+        "id":1,
+        "title":"RsSchool Certificate",
+        "caption":"RsSchool Cer",
+        "icon-src":"./images/inventory/Scroll.png",
+        "full-img-src":"./images/inventory/RsSchoolSertificate.png",
+
+    },
+    {
+        "id":1,
         "title":"Clown Diploma",
         "caption":"Bachelor Clown Diploma",
         "icon-src":"./images/inventory/Scroll.png",
